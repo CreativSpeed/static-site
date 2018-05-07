367b1f4ea1fc9b87e8e07ef4e5f9e7a6eba37472
Bachir <contact@creativspeed.com>
2018-05-07 10:00:27 +0100

    install & use Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nestedcss = require('postcss-nested');
 
 gulp.task('default', () => {
     console.log('you created a gulp task');
@@ -10,14 +14,16 @@ gulp.task('html', () => {
 });
 
 gulp.task('styles', () => {
-    console.log('Imagine sass or postCss running here.');
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nestedcss, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', () => {
     watch('./app/index.html', () => {
         gulp.start('html');
     });
-    watch('./app/styles/**/*.css', () => {
+    watch('app/assets/styles/styles.css', () => {
         gulp.start('styles');
     });
 });
