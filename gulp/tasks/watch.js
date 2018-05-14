337c9ec52b38be5c38460a32c2b001716e3c83aa
Bachir <contact@creativspeed.com>
2018-05-14 05:30:55 +0100

    Completed the reveal on scroll behavior

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,22 +2,22 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync').create();
 
-gulp.task('watch', () => {
+gulp.task('watch', function() {
     browserSync.init({
         notify: false,
         server: {
             baseDir: "app"
         }
     });
-    gulp.watch('./app/index.html', () => {
+    gulp.watch('./app/index.html', function() {
         browserSync.reload();
     });
-    gulp.watch('./app/assets/styles/**/*.css', () => {
+    gulp.watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
     });
-    gulp.watch('./app/assets/scripts/**/*..js', () => {
+    gulp.watch('./app/assets/scripts/**/*.js', function() {
         gulp.start('scriptsRefresh');
-    });
+    })
 });
 
 gulp.task('scriptsRefresh', ['scripts'],() => {
